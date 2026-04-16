a905203487be3e4459799f356f2eebe6fdb2c1cf
monynetsvlad-creator <monynetsvlad@gmail.com>
2026-04-16 22:11:43 +0300

    feat: add User, Category, and Series models

---
+++ Watchfolioo/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace Watchfolioo.Models;
+
+public class Category
+{
+    public string Name { get; set; } = "";
+    public string Tag { get; set; } = "";
+}
---
+++ Watchfolioo/Models/Series.cs
@@ -0,0 +1,10 @@
+﻿namespace Watchfolioo.Models;
+
+public class Series
+{
+    public string Title { get; set; } = "";
+    public string Genre { get; set; } = "";
+    public string Type { get; set; } = ""; 
+    public string Rating { get; set; } = "";
+    public bool IsFavorite { get; set; } = false;
+}

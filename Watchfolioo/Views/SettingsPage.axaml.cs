7c88026374a61cb6b5ba660509446f66a31429e6
monynetsvlad-creator <monynetsvlad@gmail.com>
2026-04-18 23:34:47 +0300

    feat: added WrapPanel with genres and theme switcher in SettingsPage

--- Watchfolioo/Views/SettingsPage.axaml.cs
+++ Watchfolioo/Views/SettingsPage.axaml.cs
@@ -1,12 +1,32 @@
 ﻿namespace Watchfolioo.Views;
+
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
 
 public partial class SettingsPage : UserControl
 {
+    private bool _isDark = true;
+
     public SettingsPage(string param = "")
     {
         InitializeComponent();
         if (!string.IsNullOrEmpty(param))
             ParamText.Text = $"Параметр: {param}";
+    }
+
+    private void ThemeToggle_OnClick(object? sender, RoutedEventArgs e)
+    {
+        _isDark = !_isDark;
+
+        if (Application.Current != null)
+        {
+            Application.Current.RequestedThemeVariant = _isDark
+                ? ThemeVariant.Dark
+                : ThemeVariant.Light;
+        }
+
+        ThemeLabel.Text = _isDark ? "Зараз: Темна" : "Зараз: Світла";
     }
 }
